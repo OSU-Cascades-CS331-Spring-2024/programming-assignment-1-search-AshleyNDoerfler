50b36a15c4e7d86026a270934c57395c5ebddccc
AshleyNDoerfler <AshleyNDoerfler@gmail.com>
2024-04-22 12:15:59 -0700

    Seperate words

--- bin/parse_file.py
+++ bin/parse_file.py
@@ -1,4 +1,23 @@
+import re
+from mapping import Mapping
+
 class Parse_File:
     
     def __init__(self, file_path):
         self.file_path = file_path
+        mapping = Mapping()
+
+    def main(self):
+        print('Parsing file...\n') # For testing purposes: Remove later
+
+        for line in open(self.file_path, 'r'):
+            print(line) # For testing purposes: Remove later
+
+            # ex. line: nice 43 43 12 N 7 15 59 E --> va-marseille 197
+            line = line.split(' ')
+            for word in line:
+                print(word) # For testing purposes: Remove later
+                
+
+
+
