701f8d38e94658f7dcc1e2e37896392f213de809
AshleyNDoerfler <AshleyNDoerfler@gmail.com>
2024-04-22 12:53:13 -0700

    Make get functions to get lat, lon, or connections based on a given city

--- bin/mapping.py
+++ bin/mapping.py
@@ -2,6 +2,18 @@ class Mapping:
     
     def __init__(self):
         self.city_map = {}
+        self.lat_pos = 0
+        self.lon_pos = 1
+        self.connections_pos = 2
 
     def add_city(self, city, lat, lon, connections):
         self.city_map[city] = (lat, lon, connections)
+
+    def get_citys_lat(self, city):
+        return self.city_map[city][self.lat_pos]
+    
+    def get_citys_lon(self, city):
+        return self.city_map[city][self.lon_pos]
+    
+    def get_citys_connections(self, city):
+        return self.city_map[city][self.connections_pos]
