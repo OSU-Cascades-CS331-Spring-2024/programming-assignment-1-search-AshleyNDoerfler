b56383caa7c1c5b04ec39b97fab13ec63e4f9d8d
AshleyNDoerfler <AshleyNDoerfler@gmail.com>
2024-04-22 22:55:41 -0700

    I now realise that I don't think will need this but spent too long on to leave it out of historical record

--- bin/mapping.py
+++ bin/mapping.py
@@ -19,6 +19,25 @@ class Mapping:
                 return city_obj
         else:
             return
+        
+    def set_cities_as_objects(self):
+        new_connections = []
+
+        # For each city in the city map
+        for city, connections in self.city_map.items():
+            print(connections) # For testing purposes, delete later
+            # For each connection in the city's connections
+            for connection in connections:
+                print(connection) # For testing purposes, delete later
+                connected_city_object = self.get_city(connection)
+                
+                if connected_city_object:
+                    cost = connections[connection]
+                    new_connections.append([connected_city_object, cost])
+            # Set the city's connections to the new connections list
+            self.city_map[city] = new_connections
+            new_connections = []
+        print(self.city_map) # For testing purposes, delete later
 
     def __str__(self):
         return str(self.city_map)
